1c385121979f5b0b3e62432a7f7b2f341bca83b7
Hesham <hmostafa3bd@gmail.com>
2019-04-30 21:49:39 +0200

    ShellScript

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerScript : MonoBehaviour
-{       
 
+public class PlayerScript : NetworkBehaviour {
+    
+      
+    public GameObject shell;
+    public Transform  shellSpain;
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
+
+    public float force = 6.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +24,11 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
         float translation = Input.GetAxis("Vertical") * speed;
         
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
@@ -29,5 +41,24 @@ public class PlayerScript : MonoBehaviour
        
         transform.Rotate(0, rotation, 0);
 
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            CmdFire();
+        }
+    }
+    [Command]
+    private void CmdFire()
+    {
+        GameObject  bull = (GameObject) Instantiate(shell,shellSpain.position,shellSpain.rotation); 
+        bull.GetComponent<Rigidbody>().velocity = bull.transform.forward * force;
+        NetworkServer.Spawn(bull);
+        
+        Destroy(bull,2);
+    }
+
+    public override void OnStartLocalPlayer(){
+       GetComponent<MeshRenderer>().material.color =Color.blue;
+    //    base.OnStartLocalPlayer();
     }
 }
